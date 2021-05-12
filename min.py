435ef6a4b584efb3a76739d27ea4cf2778a032e0
hn0872887 <84033062+hn0872887@users.noreply.github.com>
2021-05-12 12:09:49 -0500

    Update min.py

--- min.py
+++ min.py
@@ -1,5 +1,3 @@
 numbers = [47,95,88,73,88,84]
 
 print("Min value is: {}".format(min(numbers)))
-print("Max value is: {}".format(max(numbers)))
-print("Range: {} - {}".format(min(numbers), max(numbers)))
